0897aca6c893086f7fcd23aa1ca1e399457bf29c
Melevate <32_melevate@mail.ru>
2026-03-25 22:48:50 +0300

    add Docker support

--- backend/server.ts
+++ backend/server.ts
@@ -201,7 +201,7 @@ fastify.put<ItemUpdateRequest>('/items/:id', (request, reply) => {
 
 const port = Number(process.env.PORT) || 8080;
 
-fastify.listen({ port }, function (err, _address) {
+fastify.listen({ port, host: '0.0.0.0' }, function (err, _address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
